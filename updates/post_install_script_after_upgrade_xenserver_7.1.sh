a951d8fb066452c57b3c1a43d2cb3240cd68e7de
Yang Qian <yang.qian@citrix.com>
2018-08-16 15:32:52 +0900

    Create post_install_script_after_upgrade_xenserver_7.1.sh

---
+++ updates/post_install_script_after_upgrade_xenserver_7.1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+HOSTNAME=`cat /etc/hostname`
+host_uuid=`xe host-list --minimal name-label=$HOSTNAME`
+echo $host_uuid
+
+localstorage=`xe sr-list --minimal name-description=Cloud\ Stack\ Local\ LVM\ Storage\ Pool\ for\ $host_uuid`
+echo $localstorage
+#lvcreate VG_XenStorage-$localstorage -n LogLV -L 100G --config global{metadata_read_only=0} <<EOF
+#y
+#EOF
+#mkfs.ext3 /dev/VG_XenStorage-$localstorage/LogLV
+
+lvchange -a y /dev/VG_XenStorage-$localstorage/LogLV
+
+service rsyslog stop
+mkdir /tmp_log
+mv /var/log/* /tmp_log/
+mount /dev/VG_XenStorage-$localstorage/LogLV /var/log
+mv /tmp_log/* /var/log/
+service rsyslog start
+rm -rf /tmp_log
+
+cat <<SHEOF >> /etc/rc3.d/S02loglv
+#!/bin/bash
+lvchange -a y /dev/VG_XenStorage-$localstorage/LogLV
+SHEOF
+
+chmod a+x /etc/rc3.d/S02loglv
+
+
+
+
+wget "http://$server_ip/upgrade/updates/post_install_script_after_upgrade_xenserver_7.1.sh"
+sh post_install_script_after_upgrade_xenserver_7.1.sh

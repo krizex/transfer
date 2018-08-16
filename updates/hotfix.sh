98b05c760c9fb840b50250181b9cad7e9017e741
Yang Qian <yang.qian@citrix.com>
2018-08-16 10:58:26 +0800

    logger
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- updates/hotfix.sh
+++ updates/hotfix.sh
@@ -12,8 +12,13 @@ host_uuid=`cat /etc/xensource-inventory | grep INSTALLATION_UUID | cut -d"'" -f2
 wget "http://${server_ip}/upgrade/updates/updates.list"
 for hotfix in $(cat updates.list)
 do
+    logger -t "hotfix" "Fetching ${hotfix}.iso"
     wget "http://${server_ip}/upgrade/updates/${hotfix}.iso"
+    logger -t "hotfix" "Uploading ${hotfix}.iso"
     hotfix_uuid=`xe update-upload file-name=${hotfix}.iso 2>&1 | egrep -o [a-z0-9-]{36}`
+    logger -t "hotfix" "Applying ${hotfix}.iso"
     xe update-apply host=${host_uuid} uuid=${hotfix_uuid}
 done
+
+logger -t "hotfix" "All hotfixes applied, remove the postinstall.service"
 /usr/bin/rm -f /etc/systemd/system/multi-user.target.wants/postinstall.service

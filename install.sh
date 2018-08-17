451c0012829de62bf95d1b362acc89b92d086ee0
Yang Qian <yang.qian@citrix.com>
2018-08-17 13:41:02 +0800

    install
    Signed-off-by: Yang Qian <yang.qian@citrix.com>

--- install.sh
+++ install.sh
@@ -6,33 +6,29 @@ if [ "x$IP" == "x" ];then
     exit 1
 fi
 
-FOLDER="/var/www/html/upgrade"
+echo "Scripts will be installed..."
 
-echo "Scripts will be installed to ${FOLDER}"
+INSTALLED=""
 
-for file in $(find .)
+for file in $(find scripts)
 do
-    if [ "x${file}" == "x./install.sh" ];then
-        continue
-    fi
-
     if [ -d ${file} ]; then
         continue
     fi
 
-    d=$(dirname  ${file})
-    target_d="${FOLDER}/${d}"
+    target_f="/${file#scripts/}"
+
+    target_d=$(dirname  ${target_f})
     mkdir -p ${target_d}
-    target_f="${FOLDER}/${file}"
     echo "Installing: ${target_f}"
+    cp -f ${target_f} "${target_f}.bak"
     cp -f ${file} ${target_f}
+    INSTALLED="${INSTALLED} ${target_f}"
 done
 
-echo "Configuring IP address in scripts:"
+echo "Configuring IP address in scripts..."
 
-FILES="${FOLDER}/answerfile.xml ${FOLDER}/updates/hotfix.sh ${FOLDER}/updates/post_install.sh /var/lib/tftpboot/pxelinux.cfg/default"
-
-for f in $FILES
+for f in ${INSTALLED}
 do
     echo "=== changes of $f ==="
     sed -n -e "s/##IP##/${IP}/gp" $f
